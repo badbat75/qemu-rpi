fbb6b1e80158cc7e7de4c52ba5575d51b9794aa2
Emiliano De Simoni <emiliano.desimoni@outlook.com>
2018-12-12 23:08:16 +0100

    final version

--- scripts/kernbuild.bash
+++ scripts/kernbuild.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-QEMU_RPI_PATH=/mnt/c/Users/desimonie/Documents/git/qemu-rpi
+QEMU_RPI_PATH=${HOME}/rpi/qemu-rpi
 SRC_PATH=${HOME}/rpi/linux
 BOOT_PATH=${QEMU_RPI_PATH}/boot
 MOD_PATH=${QEMU_RPI_PATH}/modules
@@ -7,7 +7,7 @@ MOD_PATH=${QEMU_RPI_PATH}/modules
 function prepare {
     NTHREADS=$(echo $(nproc)/2+1 | bc)
     export MAKEFLAGS="-j${NTHREADS}"
-    KERNEL_VER=$(cd ${SRC_PATH}; make kernelversion)
+    KERNEL_VER=$(cd ${SRC_PATH}; make kernelversion) || exit 1
     echo "Kernel version: ${KERNEL_VER}"
     TMP_MOD_PATH=/tmp/linux-${KERNEL_VER}
     test -d "${TMP_MOD_PATH}" && rm -rf ${TMP_MOD_PATH}
@@ -27,29 +27,29 @@ cd ${SRC_PATH}
 case "${1}" in
     "raspi3")
         prepare
-        make KBUILD_CFLAGS='-O3' ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcmrpi3_defconfig
-        make KBUILD_CFLAGS='-O3' ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
-        make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=${TMP_MOD_PATH} modules_install
-        make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_DTBS_PATH=${BOOT_PATH} dtbs_install
+        make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcmrpi3_defconfig || exit 1
+        make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- || exit 1
+        make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=${TMP_MOD_PATH} modules_install || exit 1
+        make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_DTBS_PATH=${BOOT_PATH} dtbs_install || exit 1
         cp ${SRC_PATH}/arch/arm64/boot/Image.gz ${BOOT_PATH}/kernel8.img
         install
         ;;
     "raspi2")
         prepare
-        make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcm2709_defconfig
-        make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs
+        make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcm2709_defconfig || exit 1
+        make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs || exit 1
+        make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=${TMP_MOD_PATH} modules_install || exit 1
+        make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_DTBS_PATH=${BOOT_PATH} dtbs_install || exit 1
         cp ${SRC_PATH}/arch/arm/boot/zImage ${BOOT_PATH}/kernel7.img
-        make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=${TMP_MOD_PATH} modules_install
-        make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_DTBS_PATH=${BOOT_PATH} dtbs_install
         install
         ;;
     "raspi")
         prepare
-        make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcmrpi_defconfig
-        make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs
+        make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcmrpi_defconfig || exit 1
+        make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs || exit 1
+        make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=${TMP_MOD_PATH} modules_install || exit 1
+        make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_DTBS_PATH=${BOOT_PATH} dtbs_install || exit 1
         cp ${SRC_PATH}/arch/arm/boot/zImage ${BOOT_PATH}/kernel.img
-        make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=${TMP_MOD_PATH} modules_install
-        make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_DTBS_PATH=${BOOT_PATH} dtbs_install
         install
         ;;
     *)
