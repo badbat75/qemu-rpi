494969048083ef37a395ca5bd37c2d8abc2d8a24
Emiliano De Simoni <emiliano.desimoni@ynap.com>
2020-08-03 15:34:12 +0200

    enable kernel optimizations

--- scripts/kernbuild.bash
+++ scripts/kernbuild.bash
@@ -4,6 +4,7 @@
 
 QEMU_RPI_PATH=${QEMU_RPI_PATH:-${HOME}/rpi/qemu-rpi}
 SRC_PATH=${SRC_PATH:-${HOME}/rpi/linux}
+KERN_OPTLEVEL=${KERN_OPTLEVEL:-3}
 VERBOSE=${VERBOSE:-0}
 CCACHE=1
 
@@ -404,7 +405,18 @@ function kbuild {
 			CONFIG_DISABLE_FLAGS+="CONFIG_ALTERA_FREEZE_BRIDGE "
 		;;
 	esac
-	
+
+	case ${KERN_OPTLEVEL} in
+		s|S)	scripts/config -e CC_OPTIMIZE_FOR_SIZE -d CC_OPTIMIZE_FOR_PERFORMANCE -d CC_OPTIMIZE_FOR_PERFORMANCE_O3
+		;;
+		2)	scripts/config -d CC_OPTIMIZE_FOR_SIZE -e CC_OPTIMIZE_FOR_PERFORMANCE -d CC_OPTIMIZE_FOR_PERFORMANCE_O3
+		;;
+		3)	# Patch kernel to build with -O3 flag
+			sed -i '/depends on ARC$/d' init/Kconfig
+			scripts/config -d CC_OPTIMIZE_FOR_SIZE -d CC_OPTIMIZE_FOR_PERFORMANCE -e CC_OPTIMIZE_FOR_PERFORMANCE_O3
+		;;
+	esac
+
 	if [ ${ENABLELTO:-0} -eq 1 ]
 	then
 		CONFIG_ENABLE_FLAGS+="CONFIG_LTO "
